aa7c36cc140bac73df154752ae7535c7b516bd39
SagaeHaruki_ <92028294+SagaeHaruki@users.noreply.github.com>
2024-02-12 23:30:32 +0900

    Add files via upload

---
+++ BegGive/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BegGive
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int wallet = 0;
+            while (true)
+            {
+               
+                Console.WriteLine("Command List: " +
+                    "\nbeg - to beg a money" +
+                    "\ngamble - to gamble all your money" +
+                    "\nend - to end the game" +
+                    "\n");
+                Console.Write("Enter a Command: ");
+                string input = Console.ReadLine();
+                if(input == "beg")
+                {
+                    Random rand = new Random();
+                    int amt = rand.Next(0, 120);
+                    int newWallet = wallet + amt;
+                    wallet = newWallet;
+                    Console.WriteLine("You've begged and got: " + amt + " Coins!\n" +
+                        "Your new balance is: " + wallet + "\n");
+
+                }
+                else if(input == "gamble")
+                {
+                    Console.Write("Enter your winning number between 0 - 10: ");
+                    int numInpt;
+                    Random rand = new Random();
+                    int gambleNum = rand.Next(0, 10);
+                    if (int.TryParse(Console.ReadLine(), out numInpt))
+                    {
+                        Console.WriteLine("The winning number is: " + gambleNum);
+                        if (numInpt == gambleNum)
+                        {
+                            Console.WriteLine("You win!" +
+                                "\nYour balance is doubled!" +
+                                "\n");
+                            int amt = wallet + wallet;
+                            wallet = amt;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lost!" +
+                                "\n");
+                            int amt = wallet - wallet;
+                            wallet = amt;
+                        }
+                    }
+                }
+                else if(input == "end")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ GuessingGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 100);
+            Console.Write("Number of tries: ");
+            int tryCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < tryCount;)
+            {
+                Console.WriteLine("Number of tries: " + i + " of " + tryCount);
+                Console.Write("Guess a Number: ");
+                int numInpt;
+                if (int.TryParse(Console.ReadLine(), out numInpt))
+                {
+                    if (numInpt < number)
+                    {
+                        Console.WriteLine("Higher!");
+                        i++;
+                    }
+                    else if (numInpt > number)
+                    {
+                        Console.WriteLine("Lower!");
+                        i++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nice Guess! the number is: " + number);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a Valid Number:");
+                }
+            }
+        }
+    }
+}
